64a14cae5cda90664b27a90d8158ace45ad91b3a
Neto Santos <netosantosbm@gmail.com>
2022-04-01 20:23:08 -0300

    feat: created inputs app.js & style app.css

--- client/src/App.js
+++ client/src/App.js
@@ -2,35 +2,33 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App--container">
-      <div className='register--conteiner'>
-        <h1>Formulário de Cadastro</h1>
-        <input 
-        type="text" 
-        name='name' 
-        placeholder='Nome' 
-        className='register--input'
-        />
-        <input 
-        type="text" 
-        name='cost' 
-        placeholder='Preço' 
-        className='register--input'
-        />
-        <input 
-        type="text" 
-        name='category' 
-        placeholder='Categoria' 
-        className='register--input'
-        />
-        <input 
-        type="text" 
-        name='name' 
-        placeholder='Nome' 
-        className='register--input'
-        />
-      </div>
-      
+    <div className="app-container">
+    <div className="register-container">
+      <h1 className="register-title">Scrim Shop</h1>
+
+      <input
+        type="text"
+        name="name"
+        placeholder="Nome"
+        className="register-input"
+      />
+      <input
+        type="text"
+        placeholder="Preço"
+        name="cost"
+        className="register-input"
+      />
+      <input
+        type="text"
+        placeholder="Categoria"
+        name="category"
+        className="register-input"
+      />
+
+      <button className="register-button">
+        Cadastrar
+      </button>
+    </div>
     </div>
   );
 }
