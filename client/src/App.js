582a3e9190b8e5d7057db734ca9f07e6ba08cfbc
Neto Santos <netosantosbm@gmail.com>
2022-04-01 20:56:13 -0300

    feat: implamentad useState App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,21 @@
+import React, {useState} from 'react';
 import './App.css';
 
+
 function App() {
+  const [values, setValues] = useState();
+  
+  const handleChangeValues = (value) => {
+    setValues(prevValue=>({
+      ...prevValue,
+      [value.target.name]: value.target.value,
+    }))
+  };
+
+  const handleClickButton = () => {
+    console.log(values)
+  }
+
   return (
     <div className="app-container">
     <div className="register-container">
@@ -11,21 +26,24 @@ function App() {
         name="name"
         placeholder="Nome"
         className="register-input"
+        onChange={handleChangeValues}
       />
       <input
         type="text"
         placeholder="Preço"
         name="cost"
         className="register-input"
+        onChange={handleChangeValues}
       />
       <input
         type="text"
         placeholder="Categoria"
         name="category"
         className="register-input"
+        onChange={handleChangeValues}
       />
 
-      <button className="register-button">
+      <button className="register-button" onClick={() => handleClickButton()}>
         Cadastrar
       </button>
     </div>
