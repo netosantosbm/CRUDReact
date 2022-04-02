3e6ac62a40dda92d43cc766c56400eb06bccb82a
Neto Santos <netosantosbm@gmail.com>
2022-04-02 10:13:21 -0300

    feat: configured ISERT on DB /server (index.js)

--- server/index.js
+++ server/index.js
@@ -1,12 +1,42 @@
-const express = require ("express");
+const express = require("express");
 const app = express();
+const mysql = require("mysql");
+const cors = require("cors");
 
-app.get('/', (req, res) =>{
-    res.send('hello world');
-}
-);
+const db = mysql.createPool({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "dbcrud"
+});
 
+app.use(cors());
+app.use(express.json());
+app.post("/register", (req, res) => {
+    const { name } = req.body;
+    const { cost } = req.body;
+    const { category } = req.body;
 
-app.listen(3001, ()=>{
+    let SQL =  "INSERT INTO produtos ( name, cost, category ) VALUES ( ?,?,? )";
+
+    db.query(SQL, [name, cost, category], (err, result) => {
+        console.log(err);
+    });
+});
+
+app.listen(3001, () => {
     console.log("Server ON");
 });
+
+
+/*TESTE DE BANCO*/
+/* app.get('/', (req, res) => {
+    let SQL =
+        "INSERT INTO produtos ( name, cost, category ) VALUES ('Item 01','R$120', 'Alimento')";
+
+    db.query(SQL, (err, result) => {
+        console.log(err);
+    });
+}
+);
+ */
