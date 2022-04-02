dc49190444c023736d9222d58fbcda0fe526ea0e
Neto Santos <netosantosbm@gmail.com>
2022-04-02 09:37:55 -0300

    feat: add axios, configured post (App.js)

--- client/src/App.js
+++ client/src/App.js
@@ -1,52 +1,57 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import Axios from 'axios';
 
 
 function App() {
   const [values, setValues] = useState();
-  
+
   const handleChangeValues = (value) => {
-    setValues(prevValue=>({
+    setValues(prevValue => ({
       ...prevValue,
       [value.target.name]: value.target.value,
     }))
   };
 
   const handleClickButton = () => {
-    console.log(values)
+    Axios.post('http://localhost:3001/register', {
+      name: values.name,
+      cost: values.cost,
+      category: values.category,
+    });
   }
 
   return (
     <div className="app-container">
-    <div className="register-container">
-      <h1 className="register-title">Scrim Shop</h1>
+      <div className="register-container">
+        <h1 className="register-title">Scrim Shop</h1>
 
-      <input
-        type="text"
-        name="name"
-        placeholder="Nome"
-        className="register-input"
-        onChange={handleChangeValues}
-      />
-      <input
-        type="text"
-        placeholder="Preço"
-        name="cost"
-        className="register-input"
-        onChange={handleChangeValues}
-      />
-      <input
-        type="text"
-        placeholder="Categoria"
-        name="category"
-        className="register-input"
-        onChange={handleChangeValues}
-      />
+        <input
+          type="text"
+          name="name"
+          placeholder="Nome"
+          className="register-input"
+          onChange={handleChangeValues}
+        />
+        <input
+          type="text"
+          placeholder="Preço"
+          name="cost"
+          className="register-input"
+          onChange={handleChangeValues}
+        />
+        <input
+          type="text"
+          placeholder="Categoria"
+          name="category"
+          className="register-input"
+          onChange={handleChangeValues}
+        />
 
-      <button className="register-button" onClick={() => handleClickButton()}>
-        Cadastrar
-      </button>
-    </div>
+        <button className="register-button" onClick={() => handleClickButton()}>
+          Cadastrar
+        </button>
+      </div>
     </div>
   );
 }
